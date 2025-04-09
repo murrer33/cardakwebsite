d31a1b0f21fdb3e35dd2e1a7276420195ec803fb
murrer33 <berke.eutus@gmail.com>
2025-04-09 19:08:05 +0300

    Update supabaseClient.js

--- cardakwebsite/lib/supabaseClient.js
+++ cardakwebsite/lib/supabaseClient.js
@@ -1,12 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://wubahsklurblyqlurwlf.supabase.co';
-const supabaseAnonKey = 'qIcLToAc09ri2J1ob4mi7spVYM8Qtqos2ejXd7rlC1morKisNtH5IcdzWmx2nVloOf1HgfjrL2nKE6C0Fv6TVA==.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1YmFoc2tsdXJibHlxbHVyd2xmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyMTI2MDMsImV4cCI6MjA1OTc4ODYwM30.O8lEkNjYjfpoyVFADMx2ZbZMda6CGUYBv-T9BGhr0Eo';
-
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1YmFoc2tsdXJibHlxbHVyd2xmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyMTI2MDMsImV4cCI6MjA1OTc4ODYwM30.O8lEkNjYjfpoyVFADMx2ZbZMda6CGUYBv-T9BGhr0Eo';
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables. Please set NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY');
 }
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Create a single supabase client for interacting with your database
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+});
 
 export default supabase; 
